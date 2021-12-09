c9f4f2a19fed378b914353f54ab26c921ff58a89
Muhammed Koroglu <muhammed.koroglu@student.kuleuven.be>
2021-12-09 17:11:26 +0100

    added small test for maxPlayer=='O'

--- src/TestSuite.java
+++ src/TestSuite.java
@@ -36,6 +36,12 @@ public class TestSuite{
         solution = Searcher.findBestPathForMax(ticState, 3);
         assert solution.get(solution.size()-1).score() == 0;
 
+        board = new int[]{1,1,0, 1,0,-1, 0,-1,0};
+        ticState = new TicTacToeState(board, false);
+        solution = Searcher.findBestPathForMax(ticState, 1);
+        assert solution.get(solution.size()-1).score() == 0;
+        
+
         System.out.println("\nAll findBestPathForMax tests are Successful.");
     }
 
