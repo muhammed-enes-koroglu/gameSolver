7ff161ffb082c064e9c40ded36bcca2aba813bab
Muhammed Koroglu <muhammed.koroglu@student.kuleuven.be>
2022-03-06 14:08:13 +0100

    small variable name changes for readability

--- games/TurkishCheckers.java
+++ games/TurkishCheckers.java
@@ -1,15 +1,13 @@
-import java.io.StringBufferInputStream;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
-
-import javax.swing.border.Border;
 
 public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
     public static final int BOARD_SIZE = 8;
     private int[][] board;
     private boolean whitesTurn;
-    private boolean whiteIsMax;
+    private boolean maximizeForWhite;
 
     @Override
     public Set<TurkishCheckers> children() {
@@ -18,21 +16,29 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
         for(int row=0; row<BOARD_SIZE; row++){
             for(int col=0; col<BOARD_SIZE; col++){
                 int piece = this.board[row][col];
-                if(piece == 0 || !piecesTurn(piece)) continue; // implement compulsory takes.
-                int[][] childboard = this.board.clone();
-                if(!isSuper(piece))
-                    moveFwdNormalPieceAt(board, row, col);
-                // else
-                    //implement children of super pieces.
-            }
+                if(piece == 0 || !piecesTurn(piece)) continue; // look only at relevant pieces
+                
+                int[][] childBoard = this.board.clone();
+                if(isKing(piece))
+                    moveKingAt(childBoard, row, col);
+                else
+                    moveManAt(childBoard, row, col);
+                
+                children.add(new TurkishCheckers(childBoard, !this.whitesTurn, this.maximizeForWhite));
+
+                }
         }
 
 
         return children;
     }
 
-    public void moveFwdNormalPieceAt(int[][] board, int row, int col){
+    private void moveKingAt(int[][] board, int row, int col) {
+        //TODO
+    }
 
+    public void moveManAt(int[][] board, int row, int col){
+        //TODO
     }
 
     @Override
@@ -43,7 +49,7 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
 
     @Override
     public boolean isMaxPlayersTurn() {
-        return !(whiteIsMax ^ whitesTurn);
+        return !(maximizeForWhite ^ whitesTurn);
     }
 
     @Override
@@ -69,8 +75,10 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
         StringBuilder str = new StringBuilder();
         
         str.append(horizontalLineString());
-        for(int[] row: this.board)
+        for(int rowNb=BOARD_SIZE-1; rowNb>=0; rowNb--){
+            int[] row = this.board[rowNb];
             str.append(rowString(row));
+        }
         return str.toString();
     }
 
@@ -86,16 +94,16 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
         StringBuilder result = new StringBuilder();
         for(int piece: row){
             if(isWhite(piece)){
-                if(isSuper(piece))
-                    result.append("X");
+                if(isKing(piece))
+                    result.append("W");
                 else
-                    result.append("x");
+                    result.append("w");
             }
             else if(isBlack(piece)){
-                if(isSuper(piece))
-                    result.append("O");
+                if(isKing(piece))
+                    result.append("B");
                 else
-                    result.append("o");
+                    result.append("b");
             }
             else
                 result.append(" ");
@@ -108,11 +116,11 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
     
     /*
         @param: int[][] board: contains values chosen from {-2, -1, 0, 1, 2}.
-        0: empty square
-        1: normal white piece
-        -1: normal black piece
-        2: super white piece
-        -2: super black piece
+        0 : empty square
+        1 : white man
+        -1: black man
+        2 : white king
+        -2: black king
     */
     public TurkishCheckers(int[][] board, boolean whitesTurn, boolean maximizeForWhite){
         if(board.length != BOARD_SIZE)
@@ -121,31 +129,47 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
             throw new IllegalArgumentException("`board` must have " + BOARD_SIZE + " columns.");
         if(!Arrays.stream(board).allMatch(row -> 
         Arrays.stream(row).allMatch(i -> (i >= -2) && (i <= 2))))
-            throw new IllegalArgumentException("pieces can only be normal, super or dead pieces.");
-        boolean checkForWhite = true;
-        if(playerLifeThreatened(board, checkForWhite) && !Arrays.stream(board).allMatch(row -> 
-        Arrays.stream(row).allMatch(i -> i == 2)))
-            throw new IllegalArgumentException("whites should be in survival mode.");
-        if(playerLifeThreatened(board, !checkForWhite) && !Arrays.stream(board).allMatch(row -> 
-        Arrays.stream(row).allMatch(i -> i == -2)))
-            throw new IllegalArgumentException("black should be in survival mode.");
+            throw new IllegalArgumentException("pieces can only be man, king or empty.");
+
+        // // Use these ONLY while testing. VERY calculation intensive!!
+        // boolean checkForWhite = true;
+        // if(nbPiecesLeftOnBoard(board, checkForWhite) == 0 && !whitesTurn)
+        //     throw new IllegalArgumentException("It should be white's turn if white has lost.");
+        // if(nbPiecesLeftOnBoard(board, !checkForWhite) == 0 && whitesTurn)
+        //     throw new IllegalArgumentException("It should be black's turn if black has lost.");
+
+        // if(playerLifeThreatened(board, checkForWhite) && !Arrays.stream(board).allMatch(row -> 
+        // Arrays.stream(row).allMatch(i -> i == 2)))
+        //     throw new IllegalArgumentException("whites should be in survival mode.");
+        // if(playerLifeThreatened(board, !checkForWhite) && !Arrays.stream(board).allMatch(row -> 
+        // Arrays.stream(row).allMatch(i -> i == -2)))
+        //     throw new IllegalArgumentException("black should be in survival mode.");
             
         this.board = board;
         this.whitesTurn = whitesTurn;
-        this.whiteIsMax = maximizeForWhite;
+        this.maximizeForWhite = maximizeForWhite;
     }
 
     //Help methods
 
     // Checks whether player has 3 or less pieces left on the board.
     private static boolean playerLifeThreatened(int[][] board, boolean checkForWhite){
+        return nbPiecesLeftOnBoard(board, checkForWhite) <= 3;
+    }
+    
+    private static int nbPiecesLeftOnBoard(int[][] board, boolean checkForWhite){
         int nbPieces = 0;
 
         for(int[] row: board)
             for(int piece: row)
                 if(pieceBelongsToPlayer(piece, checkForWhite))
                     nbPieces++;
-        return nbPieces <= 3;
+        return nbPieces;
+    }
+
+    private static boolean pieceBelongsToPlayer(int piece, boolean isWhitePlayer){
+        int playerSign = isWhitePlayer ? 1 : -1;
+        return playerSign * piece > 0;
     }
 
     private static boolean isWhite(int piece){
@@ -156,13 +180,8 @@ public class TurkishCheckers implements TwoPersonGameState<TurkishCheckers>{
         return piece < 0;
     }
     
-    private static boolean isSuper(int piece){
+    private static boolean isKing(int piece){
         return piece == 2 || piece == -2;
-    }
-
-    private static boolean pieceBelongsToPlayer(int piece, boolean isWhitePlayer){
-        int playerSign = isWhitePlayer ? 1 : -1;
-        return playerSign * piece > 0;
     }
 
     private boolean piecesTurn(int piece){
