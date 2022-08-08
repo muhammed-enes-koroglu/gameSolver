4dafe75c6573f498eb43d1dc1e9d509163d7422e
Muhammed Köroglu <menes2000@hotmail.com>
2022-08-08 18:17:59 +0200

    Updated the requirement for the score() function

--- src/TwoPersonGameState.java
+++ src/TwoPersonGameState.java
@@ -5,7 +5,11 @@ public interface TwoPersonGameState<C>{
     /** @return A set of all possible child states that follow from this one. */
     public Set<C> children();
 
-    /** @return A number indicating how close this state is to winning. The higher the number, the closer this state is to winning. */
+    /** @return A number indicating how close the maxPlayer is to winning. 
+     * The higher the number, the closer this state is to winning. 
+     * Returns Integer.MAX_VALUE if the maxPlayer has won.
+     * Returns -Integer.MAX_VALUE if the minPlayer has won.
+     * (!!NOT Integer.MIN_VALUE) */
     public float score();
 
     /** @return Whether it is the of the player for whom we try to maximize. */
