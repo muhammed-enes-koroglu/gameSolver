e50a1ae817343adf969d94dc032033cdc6b7430a
Muhammed Köroglu <menes2000@hotmail.com>
2022-11-14 21:52:33 +0100

    Added new tests to cover situation which passes the tests, but is wron.

--- src/test/TestGameSolver.java
+++ src/test/TestGameSolver.java
@@ -25,13 +25,30 @@ public class TestGameSolver {
         ticState = new TicTacToeState(board, true);
         solution = GameSolver.findBestPathForMax(ticState, 1);
         assert solution.get(solution.size()-1).xWon();
-        
+
         // Tricky
         board = new Board( new int[]{0,-1,0, -1,0,1, 0,1,0});
         ticState = new TicTacToeState(board, true);
         solution = GameSolver.findBestPathForMax(ticState, 1);
         assert solution.get(solution.size()-1).xWon();
+
+        // Preventing O from winning.
+        board = new Board( new int[]{-1,-1,0, 1,0,0, 1,0,0});
+        ticState = new TicTacToeState(board, true);
+        solution = GameSolver.findBestPathForMax(ticState, 2);
+        board = new Board( new int[]{-1,-1,1, 1,0,0, 1,0,0});
+        ticState = new TicTacToeState(board, true);
+        assert solution.get(1).equals(ticState);
         
+        // Preventing O from winning.
+        board = new Board( new int[]{-1,1,1, 0,-1,0, 0,0,0});
+        ticState = new TicTacToeState(board, true);
+        solution = GameSolver.findBestPathForMax(ticState, 10);
+        board = new Board( new int[]{-1,1,1, 0,-1,0, 0,0,1});
+        ticState = new TicTacToeState(board, true);
+        assert solution.get(1).equals(ticState);
+
+
         // Will produce a more meaningful result when score() is improved.
         board = new Board( new int[]{0,0,0, 0,0,0, 0,0,0});
         ticState = new TicTacToeState(board, true);
