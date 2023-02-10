beb8f03841725f23521977a72b4c129bec76df9a
Muhammed Köroglu <menes2000@hotmail.com>
2023-02-10 23:31:14 +0100

    Add TestQuoridor

--- src/test/RunTests.java
+++ src/test/RunTests.java
@@ -4,6 +4,7 @@ import games.connect4.TestConnect4;
 import games.mangala.TestMangala;
 import games.reversi.TestReversi;
 import games.tictactoe.TestTicTacToeState;
+import games.quoridor.TestQuoridor;
 
 public class RunTests{
 
@@ -15,6 +16,7 @@ public class RunTests{
         TestMangala.testMangalaState();
         TestConnect4.testConnect4();
         TestReversi.testReversi();
+        TestQuoridor.testQuoridor();
 
         System.out.println("\nALL TESTS SUCCESSFULL!\n");
     }
