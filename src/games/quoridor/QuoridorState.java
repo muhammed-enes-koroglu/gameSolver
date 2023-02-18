969191439fe89b47312a1733d6ec1d1a2a2dda6d
Muhammed Köroglu <menes2000@hotmail.com>
2023-02-18 08:06:48 +0100

    Small corrections. Add a new potential score() method. Sort the explored pawn directions for optimal alpha-beta pruning

--- src/games/quoridor/QuoridorState.java
+++ src/games/quoridor/QuoridorState.java
@@ -23,8 +23,8 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
     protected static final int EMPTY = 0;
     protected static final int WALL = 1;
     protected static final int WALLABLE = -1;
-    protected static final int WHITE = 2;
-    protected static final int BLACK = -2;
+    protected static final int WHITE_PAWN = 2;
+    protected static final int BLACK_PAWN = -2;
 
     protected static final String BACKGROUND_WHITE = ConsoleColors.WHITE_BACKGROUND;
     protected static final String BACKGROUND_BLACK = ConsoleColors.BLACK_BACKGROUND;
@@ -41,19 +41,29 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
 
     @Override
     public Set<QuoridorState> children() {
-        // TODO Auto-generated method stub
-        return null;
+
+        HashSet<QuoridorState> newChildren = new HashSet<>();
+
+        // Add children for walls.
+        if((this.whitesTurn && this.whitesRemainingWalls > 0) ||
+        (!this.whitesTurn && this.blacksRemainingWalls > 0)){
+            newChildren.addAll(childrenByWall());
+        } 
+
+        addChildrenForPawn(newChildren);
+
+        return newChildren;
     }
 
     /** Return a set of children acquired by moving the pawn. */
-    private Set<QuoridorState> childrenForPawn(){
-        HashSet<QuoridorState> result = new HashSet<>();
+    private Set<QuoridorState> addChildrenForPawn(HashSet<QuoridorState> newChildren){
         Vector pawnPosition = this.whitesTurn ? this.whitePawnPosition : this.blackPawnPosition;
-        int pawn = this.whitesTurn ? WHITE : BLACK;
+        int pawn = this.whitesTurn ? WHITE_PAWN : BLACK_PAWN;
 
         // Move the pawn in all directions.
-        for(Vector direction : Vector.CARDINAL_DIRECTIONS){
-            Vector newPosition = pawnPosition.plus(direction.times(2));
+        // Sorted so we make optimal use of alpha-beta pruning.
+        for(Vector direction : getCardinalDirectionsSorted()){
+            Vector newPosition = pawnPosition.plus(direction.times(PAWN_MOVE_UNIT_SIZE));
 
             if(!this.board.locationIsOutOfBounds(newPosition)){
 
@@ -65,39 +75,55 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
 
                     // Add the new state to the result.
                     if(this.whitesTurn){
-                        result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, newPosition, this.whitePawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
+                        newChildren.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, newPosition, this.whitePawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
                     } else {
-                        result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, this.whitePawnPosition, newPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
+                        newChildren.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, this.whitePawnPosition, newPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
                     }
 
-                } else if(this.board.get(newPosition) == WHITE || this.board.get(newPosition) == BLACK){
-                    // Check if the pawn can jump over the other pawn.
-
-                    // See if there is a wall in the middle.
-                    Vector middlePosition = pawnPosition.plus(direction);
-                    if(this.board.get(middlePosition) == WALLABLE){
-                        Vector newPosition2 = newPosition.plus(direction);
-
-                        // Check if the pawn can move to the new position.
-                        if(!this.board.locationIsOutOfBounds(newPosition2) && this.board.get(newPosition2) == EMPTY){
-                                Board newBoard = this.board.copy();
-                                newBoard.set(pawnPosition, EMPTY);
-                                newBoard.set(newPosition2, pawn);
-
-                                // Add the new state to the result.
-                                if(this.whitesTurn){
-                                    result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, newPosition2, this.whitePawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
-                                } else {
-                                    result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, this.whitePawnPosition, newPosition2, newBoard, !this.whitesTurn, this.maximizeForWhite));
-                                }
-
-                            }
-                    }
-                }
+                } else
+                    addChildrenIfJumpPossible(newChildren, pawnPosition, pawn, direction, newPosition);
             }
         }
 
-        return result;
+        return newChildren;
+    }
+
+    /** Add new children if the current pawn can make a jump. */
+    private void addChildrenIfJumpPossible(HashSet<QuoridorState> newChildren, Vector pawnPosition, int pawn, Vector direction,
+            Vector newPosition) {
+
+        // Check if the pawn can jump over the other pawn.
+        if(this.board.get(newPosition) == WHITE_PAWN || this.board.get(newPosition) == BLACK_PAWN){
+
+            // See if there is a wall in the middle.
+            Vector middlePosition = newPosition.plus(direction);
+            if(this.board.get(middlePosition) == WALLABLE){
+                // There is another pawn in the new position, and no wall in the middle, 
+                // so the pawn will jump.
+                Vector positionAfterJump = newPosition.plus(direction.times(PAWN_MOVE_UNIT_SIZE));
+
+                // Check if the pawn can move to the new position.
+                if(!this.board.locationIsOutOfBounds(positionAfterJump) && 
+                this.board.get(positionAfterJump) == EMPTY){
+                    // We actually don't even need to check 
+                    // if the positionAfterJump is empty.
+                    // That's because we know that this pawn isn't there,
+                    // and the other pawn is next to this pawn.
+
+                    Board newBoard = this.board.copy();
+                    newBoard.set(pawnPosition, EMPTY);
+                    newBoard.set(positionAfterJump, pawn);
+
+                    // Add the new state to the result.
+                    if(this.whitesTurn){
+                        newChildren.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, positionAfterJump, this.whitePawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
+                    } else {
+                        newChildren.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls, this.whitePawnPosition, positionAfterJump, newBoard, !this.whitesTurn, this.maximizeForWhite));
+                    }
+
+                }
+            }
+        }
     }
 
     /** Return a set of children acquired by placing a wall. */
@@ -114,24 +140,9 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         for(Vector direction : Vector.CARDINAL_DIRECTIONS){
             for(int i = 0; i < BOARD_SIZE; i++){
                 for(int j = 0; j < BOARD_SIZE; j++){
-                    Vector position = new Vector(i, j);
-
-                    // Check if the wall can be placed here.
-                    if(this.board.get(position) == WALLABLE){
-                        if(this.canPlaceWall(position, direction)){
-                            Board newBoard = this.board.copy();
-                            newBoard.set(position, WALL);
-                            newBoard.set(position.plus(direction), WALL);
-                            newBoard.set(position.plus(direction.times(2)), WALL);
-
-                            // Add the new state to the result.
-                            if(this.whitesTurn){
-                                result.add(new QuoridorState(this.whitesRemainingWalls - 1, this.blacksRemainingWalls, this.whitePawnPosition, this.blackPawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
-                            } else {
-                                result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls - 1, this.whitePawnPosition, this.blackPawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
-                            }
-                        }
-                    }
+                    
+                    Vector wallStartingPosition = new Vector(i, j);
+                    addChildrenIfWallPossible(result, direction, wallStartingPosition);
                 }
             }
         }
@@ -139,19 +150,33 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         return result;
     }
 
+    private void addChildrenIfWallPossible(HashSet<QuoridorState> result, Vector direction, Vector wallStartingPosition) {
+
+        // Check if the wall can be placed here.
+        if(this.canPlaceWall(wallStartingPosition, direction)){
+            Board newBoard = this.board.copy();
+
+            // Place the wall.
+            for(int k=WALL_SIZE-1; k>=0; k--){
+                newBoard.set(wallStartingPosition.plus(direction.times(k)), WALL);
+            }
+
+            // Add the new state to the result.
+            if(this.whitesTurn){
+                result.add(new QuoridorState(this.whitesRemainingWalls - 1, this.blacksRemainingWalls, this.whitePawnPosition, this.blackPawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
+            } else {
+                result.add(new QuoridorState(this.whitesRemainingWalls, this.blacksRemainingWalls - 1, this.whitePawnPosition, this.blackPawnPosition, newBoard, !this.whitesTurn, this.maximizeForWhite));
+            }
+        }
+    }
+
     /** Return true if a wall can be placed at the given position in the given direction. */
     private boolean canPlaceWall(Vector position, Vector direction){
         // Check if the wall can be placed here.
-        if(this.board.get(position) != WALLABLE){
-            return false;
-        }
-        if(!this.board.locationIsOutOfBounds(position.plus(direction)) && 
-            this.board.get(position.plus(direction)) != WALLABLE){
-            return false;
-        }
-        if(!this.board.locationIsOutOfBounds(position.plus(direction.times(2))) && 
-            this.board.get(position.plus(direction.times(2))) != WALLABLE){
-            return false;
+        for(int i=WALL_SIZE-1; i>=0; i--){
+            if(this.board.locationIsOutOfBounds(position.plus(direction.times(i)))){
+                return false;
+            }
         }
 
         // Check if the wall blocks the path of the other pawn. Use QuoridorPathFinder.
@@ -164,11 +189,8 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         // Check if the wall blocks the path of the own pawn. Use QuoridorPathFinder.
         Vector ownPawnPosition = this.whitesTurn ? this.whitePawnPosition : this.blackPawnPosition;
         int ownPawnTargetRow = this.whitesTurn ? TARGET_ROW_BLACK : TARGET_ROW_WHITE;
-        if(!QuoridorPathFinder.pathExists(this.board, ownPawnPosition, ownPawnTargetRow)){
-            return false;
-        }
 
-        return true;
+        return QuoridorPathFinder.pathExists(this.board, ownPawnPosition, ownPawnTargetRow);
     }
 
     @Override
@@ -179,6 +201,11 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         int result = 0;
         result += TARGET_ROW_WHITE - this.whitePawnPosition.row;
         result -= this.blackPawnPosition.row - TARGET_ROW_BLACK;
+
+        // A more advanced score function.
+        // But uses a LOT more time.
+        // result += QuoridorPathFinder.pathLength(this.board, this.whitePawnPosition, TARGET_ROW_WHITE);
+        // result -= QuoridorPathFinder.pathLength(this.board, this.blackPawnPosition, TARGET_ROW_BLACK);
 
         if(this.maximizeForWhite){
             return result;
@@ -232,11 +259,11 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
 
         // Print the board using the PrintBoardGame class.
         Map<Integer, String> pieceRepresentations = Map.of(
-            BLACK, BACKGROUND_BLACK + "O" + backgroundColor,
+            BLACK_PAWN, BACKGROUND_BLACK + "O" + backgroundColor,
             WALLABLE, ConsoleColors.BLUE_BACKGROUND + " " + backgroundColor,
             EMPTY, " ",
             WALL, ConsoleColors.RED_BACKGROUND + " " + backgroundColor,
-            WHITE, BACKGROUND_WHITE + "X" + backgroundColor
+            WHITE_PAWN, BACKGROUND_WHITE + "X" + backgroundColor
         );
         
         String turnMarker = this.whitesTurn ? BACKGROUND_WHITE + "X" : BACKGROUND_BLACK + "O";
@@ -260,8 +287,8 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         this.blackPawnPosition = new Vector(BOARD_SIZE-1, (BOARD_SIZE - 1) / 2);
 
         this.board = getInitialBoard();
-        this.board.set(whitePawnPosition, WHITE);
-        this.board.set(blackPawnPosition, BLACK);
+        this.board.set(whitePawnPosition, WHITE_PAWN);
+        this.board.set(blackPawnPosition, BLACK_PAWN);
 
         this.whitesTurn = true;
         this.maximizeForWhite = true;
@@ -286,10 +313,10 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
         if(whitePawnPosition.row % PAWN_MOVE_UNIT_SIZE != 0 || whitePawnPosition.col % PAWN_MOVE_UNIT_SIZE != 0) {
             throw new IllegalArgumentException("whites pawn must be on a movable position");
         }
-        if(board.get(whitePawnPosition) != WHITE) {
+        if(board.get(whitePawnPosition) != WHITE_PAWN) {
             throw new IllegalArgumentException("white's pawn must be on `whitePawnPosition`");
         }
-        if(board.get(blackPawnPosition) != BLACK) {
+        if(board.get(blackPawnPosition) != BLACK_PAWN) {
             throw new IllegalArgumentException("black's pawn must be on `blackPawnPosition`");
         }
 
@@ -323,6 +350,19 @@ public class QuoridorState implements TwoPersonGameState<QuoridorState> {
 
     public Vector getBlackPawnPosition() {
         return blackPawnPosition;
+    }
+
+    /** Returns a vector array ordered either UP, RIGHT, LEFT, DOWN;
+     * or DOWN, LEFT, RIGHT, UP.
+     * We do this because the order of the directions explored decides 
+     * how much use of alpha-beta pruning we can make.
+     */
+    public Vector[] getCardinalDirectionsSorted(){
+        if(this.whitesTurn){
+            return new Vector[]{Vector.UP, Vector.RIGHT, Vector.LEFT, Vector.DOWN};
+        } else {
+            return new Vector[]{Vector.DOWN, Vector.LEFT, Vector.RIGHT, Vector.UP};
+        }
     }
 
 }
