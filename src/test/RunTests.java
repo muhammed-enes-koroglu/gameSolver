28c4155fd67c1b3217f2e3e7ae958c6226df3dea
Muhammed Köroglu <menes2000@hotmail.com>
2024-04-16 13:38:55 +0200

    Add test average number of children in RunTests.java

--- src/test/RunTests.java
+++ src/test/RunTests.java
@@ -1,9 +1,16 @@
 package test;
 
+import java.util.List;
+
 import games.connect4.TestConnect4;
+import games.mangala.MangalaState;
+import games.mangala.PlayMangala;
 import games.mangala.TestMangala;
 import games.reversi.TestReversi;
 import games.tictactoe.TestTicTacToeState;
+import util.ConsoleColors;
+import static util.ConsoleColors.pprint;
+
 
 public class RunTests{
 
@@ -11,12 +18,35 @@ public class RunTests{
 
         System.out.println("[TESTING]\n");
         TestTicTacToeState.testTicTacToeState();
+        TestGameAI.testAStarMinimax();
         TestGameSolver.testIterDeepeningMiniMax();
         TestMangala.testMangalaState();
         TestConnect4.testConnect4();
         TestReversi.testReversi();
 
+
         System.out.println("\nALL TESTS SUCCESSFULL!\n");
+
+        someTest();
+    }
+
+
+    /**
+     * This method performs a test on the Mangala game solver.
+     * It initializes the game state, sets the number of generations and iterations,
+     * and calculates the average children count for each generation.
+     * Finally, it prints the average children count to the console.
+     */
+    public static void someTest(){
+        MangalaState initialState = new PlayMangala().getInitialState(true);
+        int nbGenerations = 150;
+        int numIterations = 1000;
+        
+        List<Float> avgChildrenCount = Statistics.getAverageChildrenCounts(numIterations, nbGenerations, initialState);
+
+        // Print the average children count
+        pprint(ConsoleColors.CYAN_BRIGHT, "Average children count for each generation:");
+        pprint(ConsoleColors.CYAN_BRIGHT, avgChildrenCount.toString());
     }
 
 }
