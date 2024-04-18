48a707d3f8e46189e4025a4adfc2b80d07b07522
Muhammed Köroglu <menes2000@hotmail.com>
2024-04-18 12:20:51 +0200

    Refactor Vector class to add toString() method

--- src/util/Vector.java
+++ src/util/Vector.java
@@ -58,4 +58,9 @@ public class Vector {
     public int hashCode() {
         return 31 * row + col;
     }
+
+    @Override
+    public String toString(){
+        return "(" + row + ", " + col + ")";
+    }
 }
