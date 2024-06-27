5c0dddbe6ecf84013e869be505a893293d81c26d
Muhammed Köroglu <menes2000@hotmail.com>
2024-06-27 20:48:53 +0200

    chore: Refactor TestGameAI to use Node<TicTacToeState> instead of TicTacToeState

--- src/test/TestGameAI.java
+++ src/test/TestGameAI.java
@@ -1,6 +1,7 @@
 package test;
 
 import util.Board;
+import util.Node;
 
 import java.util.List;
 import games.tictactoe.TicTacToeState;
@@ -15,11 +16,11 @@ public class TestGameAI {
 
     public static void testAStarMinimax() {
         System.out.println("[TESTING] AStarMinimax");
-        float minSearchTime = 1f;
+        int maxDepth = 4;
 
         Board board;
         TicTacToeState ticState;
-        List<TicTacToeState> solution;
+        List<Node<TicTacToeState>> solution;
 
         // Test scenario configurations
         int[][] testBoards = {
@@ -37,27 +38,27 @@ public class TestGameAI {
         for (int i = 0; i < testBoards.length; i++) {
             board = new Board(testBoards[i]);
             ticState = new TicTacToeState(board, isMaxTurn[i]);
-            solution = AStarSearch.aStarMinimax(ticState, minSearchTime);
+            solution = AStarSearch.search(ticState, maxDepth).getPath();
             
             switch (i) {
                 case 0: case 1: case 2:  // Tests where X is expected to win
-                    assert solution.get(solution.size() - 1).xWon();
+                    assert solution.get(solution.size() - 1).getState().xWon();
                     break;
                 case 3:  // Preventing O from winning, next move should be a win
                     board = new Board(new int[]{-1,-1,1, 1,0,0, 1,0,0});
                     ticState = new TicTacToeState(board, true);
-                    assert solution.get(1).equals(ticState);
+                    assert solution.get(1).getState().equals(ticState);
                     break;
                 case 4:  // Another preventing O from winning scenario
                     board = new Board(new int[]{-1,1,1, 0,-1,0, 0,0,1});
                     ticState = new TicTacToeState(board, true);
-                    assert solution.get(1).equals(ticState);
+                    assert solution.get(1).getState().equals(ticState);
                     break;
                 case 5:  // Neutral start state
-                    assert solution.get(solution.size() - 1).score() == 0;
+                    assert solution.get(solution.size() - 1).getState().score() == 0;
                     break;
                 case 6:  // Test where O is losing
-                    assert solution.get(solution.size() - 1).score() == TwoPersonGameState.MIN_SCORE;
+                    assert solution.get(solution.size() - 1).getState().score() == TwoPersonGameState.MIN_SCORE;
                     break;
                 default:  // Default case
                     System.out.println("Unexpected value: " + i);
